d7c1eceb699d090d5b9414d05c4ec466b2390ce5
steve <2226069315@qq.com>
2019-07-29 12:27:20 +0800

    test class rename

--- src/main/java/steve/spring/springbootlearn/SpringBootLearnApplication.java
+++ src/main/java/steve/spring/springbootlearn/SpringBootLearnApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootlearnApplication {
+public class SpringBootLearnApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootlearnApplication.class, args);
+        SpringApplication.run(SpringBootLearnApplication.class, args);
     }
 
 }
